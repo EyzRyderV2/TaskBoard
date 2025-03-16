f04c21ed22ba2359410f9921177a89fbf40cf3b0
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-03-16 18:21:15 -0300

    config: remove spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.eyz"
@@ -24,17 +22,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.liquibase:liquibase-core:4.29.1")
 
 	compileOnly("org.projectlombok:lombok:1.18.34")
 
 	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
+	runtimeOnly("com.mysql:mysql-connector-j:8.2.0")
 
 	annotationProcessor("org.projectlombok:lombok")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
