a58df511195c8b0ccabebb4c1c556fbce890be49
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-03-16 21:31:37 -0300

    feat: add card service and main and board menu

---
+++ src/main/java/com/eyz/TaskBoard/service/BoardColumnQueryService.java
@@ -0,0 +1,18 @@
+package com.eyz.TaskBoard.service;
+import com.eyz.TaskBoard.persistence.dao.BoardColumnDAO;
+import com.eyz.TaskBoard.persistence.entity.BoardColumnEntity;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardColumnQueryService {
+    private final Connection connection;
+
+    public Optional<BoardColumnEntity> findById(final Long id) throws SQLException {
+        var dao = new BoardColumnDAO(connection);
+        return dao.findById(id);
+    }
+}
